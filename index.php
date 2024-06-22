194f7e9806834c5d89eccdd7318e356d7c5f3298
ramadhani14 <drama2713@gmail.com>
2024-06-23 00:08:23 +0700

    aaa

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 $menu = "beranda";
 include 'header.php';
 ?>
+<video width="100%" height="800" controls>
+  <source src="video/video.mp4" type="video/mp4">
+  <source src="video/video.ogg" type="video/ogg">
+Your browser does not support the video tag.
+</video>
     <selection class="grid">
         <div class="container">
             <div class="content">
